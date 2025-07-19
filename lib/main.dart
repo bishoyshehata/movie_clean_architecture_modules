24c6b167df712e9f68c45ee691254d66b57ed4db
Bishoy Shehata <bishoyshehata9@gmail.com>
2025-07-19 23:39:07 +0300

    connect main to navigator

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:login/presentation/screens/login_screen.dart';
 import 'package:movie_clean_architecture_modules/di/injection.dart';
+import 'package:navigator/navigation_module.dart';
 
 Future<void> main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await configureDependencies(Environment.prod);
-  runApp(const MyApp());
+  runApp(const NavigationModule());
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
-      title: 'Flutter Demo',
-      home:  LoginScreen(),
-    );
-  }
-}
-
