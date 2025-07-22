30897a0aca29e218bed57d3defa7b20f49d0e809
Bishoy Shehata <bishoyshehata9@gmail.com>
2025-07-22 23:50:48 +0300

    update movies module with box & cached data source

--- features/movies/lib/data/di/movie_module.dart
+++ features/movies/lib/data/di/movie_module.dart
@@ -1,6 +1,10 @@
 import 'package:data/network_info/network_info.dart';
 import 'package:dio/dio.dart';
+import 'package:hive/hive.dart';
 import 'package:injectable/injectable.dart';
+import 'package:movies/data/datasource/cache/movies_cached_datasource.dart';
+import 'package:movies/data/datasource/cache/movies_cached_datasource_impl.dart';
+import 'package:movies/domain/model/cached_movie_model.dart';
 import '../../domain/repository/movies_repository.dart';
 import '../../domain/usecase/movies_usecase.dart';
 import '../datasource/remote/movies_datasource.dart';
@@ -10,6 +14,12 @@ import '../service/movies_service.dart';
 
 @module
 abstract class MoviesModule {
+  @preResolve
+  Future<Box<dynamic>> provideMoviesBox()async {
+    Hive.registerAdapter(CachedMovieModelAdapter());
+    return Hive.openBox<dynamic>("moviesBox");
+  }
+
   @lazySingleton // when we need it ,we have to provide it
   MoviesService provideMoviesService(Dio dio){
     return MoviesService(dio);
@@ -20,8 +30,12 @@ abstract class MoviesModule {
     return MoviesDataSourceImpl(moviesService, networkInfo);
   }
   @lazySingleton // when we need it ,we have to provide it
-  MoviesRepository provideMoviesRepository(MoviesDataSource moviesDataSource){
-    return MoviesRepositoryImpl(moviesDataSource);
+  MoviesCachedDataSource provideMoviesCachedDataSource(Box<dynamic> box){
+    return MoviesCachedDataSourceImpl(box);
+  }
+  @lazySingleton // when we need it ,we have to provide it
+  MoviesRepository provideMoviesRepository(MoviesDataSource moviesDataSource ,MoviesCachedDataSource moviesCachedDataSource){
+    return MoviesRepositoryImpl(moviesDataSource,moviesCachedDataSource);
   }
   @lazySingleton // when we need it ,we have to provide it
   MoviesUseCase provideMoviesUseCase(MoviesRepository moviesRepository){
--- features/movies/lib/data/repository/movies_repository_impl.dart
+++ features/movies/lib/data/repository/movies_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:domain/entities/failure.dart';
+import 'package:movies/data/datasource/cache/movies_cached_datasource.dart';
 import 'package:movies/domain/mapper/movies_mapper.dart';
 import '../../domain/model/movie_model.dart';
 import '../../domain/repository/movies_repository.dart';
@@ -7,7 +8,8 @@ import '../datasource/remote/movies_datasource.dart';
 
 class MoviesRepositoryImpl implements MoviesRepository {
   final MoviesDataSource moviesDataSource;
-  MoviesRepositoryImpl(this.moviesDataSource);
+  final MoviesCachedDataSource moviesCachedDataSource;
+  MoviesRepositoryImpl(this.moviesDataSource,this.moviesCachedDataSource);
   @override
   Future<Either<Failure, List<MovieModel>>> getMovies() async {
     final result = await moviesDataSource.getMovies();
